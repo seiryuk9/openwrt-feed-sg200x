c85d2de4894631f543311c1e6fb8f89999205cb8
Phuong Nguyen <ndp2809@gmail.com>
2025-03-16 07:54:49 +0700

    remove target image tmp folder

--- target/linux/sg200x/image/tmp/root/mnt/system/auto.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Put the program you want to run automatically here
-
--- target/linux/sg200x/image/tmp/root/mnt/system/usr/bin/camera-test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export LD_LIBRARY_PATH=/mnt/system/lib
-
-sample_vi_fd /mnt/cvimodel/scrfd_768_432_int8_1x.cvimodel
-
